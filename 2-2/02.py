9f13b581dc551e12dac6f896a2edfd41fe2a61f6
ali azizi <azizi.ali87@gmail.com>
2022-04-05 18:03:17 -0700

    04052203

--- 2-2/02.py
+++ 2-2/02.py
@@ -2,6 +2,17 @@
 
 import turtle
 
+# Define program constants 
+WIDTH = 500
+HEIGHT = 500
+
+# Create a window where we can show our drawing
+
+screen = turtle.Screen()
+screen.setup(WIDTH,HEIGHT)
+screen.title("Welcome to my turtle's home")
+screen.bgcolor("cyan")
+
 # Creating an instance
 
 my_turtle = turtle.Turtle()
@@ -9,10 +20,10 @@ my_turtle = turtle.Turtle()
 
 # our turtle attributes
 
-my_turtle.color("blue")
+my_turtle.color("red")
 my_turtle.shape("turtle")
 my_turtle.forward(200)
-my_turtle.speed(10)
+# my_turtle.speed(10)
 
 # This statement is needed at the end of all turtle programs.
 turtle.done()
