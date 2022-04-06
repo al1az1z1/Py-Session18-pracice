74c9e75c79c4604a00372b311ba1472574bca396
ali azizi <azizi.ali87@gmail.com>
2022-04-05 17:54:35 -0700

    03

---
+++ 2-2/02.py
@@ -0,0 +1,18 @@
+# Importing Turtle Graphic module
+
+import turtle
+
+# Creating an instance
+
+my_turtle = turtle.Turtle()
+
+
+# our turtle attributes
+
+my_turtle.color("blue")
+my_turtle.shape("turtle")
+my_turtle.forward(200)
+my_turtle.speed(10)
+
+# This statement is needed at the end of all turtle programs.
+turtle.done()
