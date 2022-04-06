257bff073d47cddb42e8790b49a5dd5896d1b9cc
ali azizi <azizi.ali87@gmail.com>
2022-04-05 17:47:40 -0700

    02

---
+++ 2-2/01.py
@@ -0,0 +1,16 @@
+#Importing turtle Graphic module
+from turtle import *
+
+color("red")
+forward(100)
+right(90)
+forward(100)
+right(90)
+forward(100)
+right(90)
+forward(100)
+right(90)
+
+
+done()
+
